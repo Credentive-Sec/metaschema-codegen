96cd3a259b41845e1953d2668d5108984b8787d9
Robert Sherwood <robert.sherwood@credentive.com>
2024-06-08 13:53:19 +0000

    Added docstrings for packages

---
+++ metaschema-python/metaschema_python/core/__init__.py
@@ -0,0 +1,5 @@
+"""
+The Core module provides packages for parsing metaschema specifications.
+
+It also contains the base classes that are subclassed to represent elements defined in a specific metaschema.
+"""
