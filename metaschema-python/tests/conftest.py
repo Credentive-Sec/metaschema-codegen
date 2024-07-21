d11cb4bd65b3b3cbea1b96a941cc4de193e94fb5
Robert Sherwood <robert.sherwood@credentive.com>
2024-07-21 09:37:54 +0000

    test fixtures

--- metaschema-python/tests/conftest.py
+++ metaschema-python/tests/conftest.py
@@ -1,12 +1,18 @@
 import pytest
 
 from metaschema_python.core.schemaparse import MetaschemaParser, MetaSchemaSet
+from metaschema_python.codegen.generate_classes import PackageGenerator, Package
+from pathlib import Path
 
 
 def build_metaschema_set() -> MetaSchemaSet:
     return MetaschemaParser.parse(
         metaschema_location="/workspaces/metaschema-python/OSCAL/src/metaschema/oscal_complete_metaschema.xml"
     )
+
+
+def package_generator(metaschema_set: MetaSchemaSet) -> PackageGenerator:
+    return PackageGenerator(metaschema_set, Path("/tmp/metaschema_python_ptest/"))
 
 
 @pytest.fixture(scope="module")
@@ -18,3 +24,8 @@ def parsed_metaschema(pytestconfig):
     #     pytestconfig.cache.set("metaschema", metaschema)
     # return metaschema
     return build_metaschema_set()
+
+
+@pytest.fixture(scope="module")
+def generated_package(parsed_metaschema):
+    return package_generator(parsed_metaschema)
