1cd3b7097d299e0a5adc4153a7e2b6027908fd4d
Robert Sherwood <robert.sherwood@credentive.com>
2024-07-21 09:36:41 +0000

    refactor class generator

--- metaschema-python/metaschema_python/codegen/generate_classes.py
+++ metaschema-python/metaschema_python/codegen/generate_classes.py
@@ -1,40 +1,96 @@
+from __future__ import annotations
+
 import jinja2
-from typing import TypeAlias
+from dataclasses import dataclass
+from typing import NamedTuple, cast, Literal
+from pathlib import Path
 
-from ..core.schemaparse import MetaSchemaSet
+from ..core.schemaparse import MetaSchemaSet, MetaSchema
 
 
-def _get_class_name(instance: str) -> str:
+def _pythonize_name(name: str) -> str:
     """
-    Returns the name of the Class that will instantiate a defined assembly, field or flag in a module.
-    This is provided to ensure consistent names when translating from fields to anything else
+    Returns the name of the class or variable for a defined assembly, field or flag in a module.
+    Makes the name python safe by stripping spaces and converts dashes to underscores.
+    This is provided to ensure consistent names when translating from fields to anything else.
     """
     # Strip spaces, convert dashes to underscores
-    return f'{instance.replace(" ", "").replace("-","_")}'
+    return f'{name.replace(" ", "").replace("-","_")}'
+
+
+class GlobalRef(NamedTuple):
+    """
+    A NamedTuple to represent an element exported from a metaschema by being tagged "global". This is used to simplify
+    generation of import statements and references in one module to classes defined in another module.
+
+    Elements:
+        NamedTuple (_type_): _description_
+        schema_source: (str):  the metaschema defining the global
+        module_name: (str): the name of the module where this ref will be located
+        ref_name: (str): the name of the property that will be the "@ref"
+        class_name: (str): the name of the class that will represent the element referenced
+    """
+
+    schema_source: str
+    module_name: str
+    ref_name: str
+    class_name: str
+
+
+class Document(NamedTuple):
+    """
+    A Class to represent a document that containts a Metaschema compliant structure, used to collect all the metaschema
+    assemblies with a 'root-name' attribute. This class is not referenced directly in metascheama, but must exist for a
+    root assembly to be JSON serialized by python.
+    """
+
+    root_elements: list[str]
 
 
 class PackageGenerator:
     """
-    This class takes a dict[SchemaID, MetaSchema] and generates a package with Python source code for each
-    metaschemas present in the dictionary.
+    This class is initialized with a MetaSchemaSet and generates a package with Python source code for each of the metaschemas.
     """
 
-    def __init__(self, parsed_metaschemas: MetaSchemaSet) -> None:
-        self.classes: list[ModuleGenerator] = []
+    def __init__(
+        self, parsed_metaschemas: MetaSchemaSet, destination_directory: Path
+    ) -> None:
+        # initialize the package
+        self.destination = destination_directory
+        self.module_generators: list[ModuleGenerator] = []
 
-        # We process this in two passes. First we extract all the globals from all the items in the MetaSchemaSet,
-        # then we call ClassGenerator on each class
-        self.globals: dict[str, list[str]] = {}
+        # Get the global symbols from all the metaschemas in the MetaschemaSet
+        self.global_refs: list[GlobalRef] = []
+        for metaschema in parsed_metaschemas.metaschemas:
+            schema_source = str(metaschema.file)
+            module_name = _pythonize_name(metaschema.short_name)
+            for global_ref_key in metaschema.globals:
+                self.global_refs.append(
+                    GlobalRef(
+                        schema_source=schema_source,
+                        module_name=module_name,
+                        ref_name=global_ref_key,
+                        class_name=metaschema.globals[global_ref_key],
+                    )
+                )
+
+        # add references for metaschema datatypes, they look a little strange because they're in the metaschema xsd,
+        # not a specific metaschema
+        for datatype in parsed_metaschemas.datatypes:
+            self.global_refs.append(
+                GlobalRef(
+                    schema_source="datatype",
+                    module_name="datatypes",
+                    ref_name=datatype.name,
+                    class_name=datatype.name,
+                )
+            )
 
         for metaschema in parsed_metaschemas.metaschemas:
-            
-
-        # Next, we parse the metaschemas, passing in the globals dict
-        for metaschema in parsed_metaschemas.values():
-            self.classes.append(
+            self.module_generators.append(
                 ModuleGenerator(
                     metaschema=metaschema,
-                    globals=self.globals,
+                    global_refs=self.global_refs,
                 )
             )
 
@@ -45,78 +101,111 @@ class PackageGenerator:
         """
         pass
 
+    def _pythonized_export_names(self, metaschema: MetaSchema) -> list[str]:
+        """
+        given a list of global symbols from a metaschema, convert it to a python safe string of the form "method.Class" for reference in other python strings
 
-class Document:
-    """
-    A Class to represent a document that containts a Metaschema compliant structure.
-    This is used to collect all the metaschema assemblies with a 'root-name' attribute.
-    """
+        Args:
+            metaschema (MetaSchema): the Metaschema to be processed
 
-    root_elements: list[str]
+        Returns:
+            list[str]: a list of exported functions from the module in python safe format
+        """
+        # convert short name to a python module name
+        module_name = _pythonize_name(metaschema.short_name)
+
+        # convert all element names to python safe names, and prepend the module name
+        class_names = [
+            f"{module_name}.{_pythonize_name(_class)}" for _class in metaschema.globals
+        ]
+        return class_names
 
 
 class ModuleGenerator:
     """
-    A class to generate python source code from a parsed metaschema
+    A class to generate python source code from a parsed metaschema.
+    It converts a data object representing a generic metaschema to a python oriented dictionary to pass to a template.
     """
 
-    def __init__(self, metaschema: MetaSchemaSet) -> None:
-        self.metaschema_dict = metaschema
-        self.version = metaschema["schema-version"]
-        self.module_name = metaschema["short-name"]
-        self.assemblies = []
-        self.fields = []
-        self.flags = []
-        self.imported_globals: GlobalsDictType = {}
-        self.local_methods: dict[str, str] = {}
+    def __init__(self, metaschema: MetaSchema, global_refs: list[GlobalRef]) -> None:
+        self.metaschema = metaschema
+        self.version = cast(str, self.metaschema["schema-version"])
+        self.module_name = _pythonize_name(cast(str, self.metaschema["short-name"]))
+        self.module_imports = []
+        self.class_generators: list[ClassGenerator] = []
+        self.source = ""
 
-        # TODO: check the "import" for items with scope "global" or no scope (global is the default) - add them to the list of assemblies that can be referenced
-        for sub_schema in self.metaschema_dict.get("import", []):
-            # Create subset of globals that are relevant to our scope
-            import_filename = sub_schema.get("@href")
-            self.imported_globals[import_filename] = globals[import_filename]
+        #
+        # The first pass is to generate the list of elements imported by or defined in the metaschema so that we can
+        # identify the appropriate class for a "@ref"
+        #
+
+        # get a list of elements from imports that could be referenced with a "@ref" in this module
+        imported_schemas = [
+            _import["@href"] for _import in self.metaschema.get("import", [])
+        ]
+
+        module_refs = {}
+        for schema in imported_schemas:
+            module_refs.update(
+                {
+                    g_ref.ref_name: g_ref.class_name
+                    for g_ref in global_refs
+                    if g_ref.schema_source == schema
+                }
+            )
+
+        module_refs.update(self._get_local_refs())
+
+        #
+        # With our ref dictionary in place, we can parse through the underlying elements.
+        #
 
         # TODO: parse out definitions of fields, flags and assemblies
-        for assembly in self.metaschema_dict.get("define-assembly", []):
-            self.assemblies.append(self._generate_assembly(assembly_dict=assembly))
+        for assembly in self.metaschema.get("define-assembly", []):
+            self.class_generators.append(
+                self._generate_assembly(assembly_dict=assembly)
+            )
 
-        for field in self.metaschema_dict.get("define-field", []):
+        for field in self.metaschema.get("define-field", []):
             self.fields.append(self._generate_field(field_dict=field))
 
-        for flag in self.metaschema_dict.get("define-flag", []):
+        for flag in self.metaschema.get("define-flag", []):
             self.flags.append(self._generate_flag(flag_dict=flag))
 
         # TODO: iterate through definitions and pass them to the appropriate jinja templates. for "@ref", include an import and reference. check global imports.
 
-    def _get_local_methods(self) -> None:
+    def _get_local_refs(self) -> dict[str, str]:
         """
-        Returns the local symbols defined in this metaschema, so that we can dereference "@refs"
+        Returns the local symbols defined in this metaschema, so that we can dereference "@refs".  This is the first pass.
         """
+        local_refs = {}
 
-        # return empty list if key does not exist
-        for assembly in self.metaschema_dict.get("define-assembly", []):
-            self.local_methods[assembly["@name"]] = (
-                f'{self.metaschema_dict._get_class_name(assembly["formal-name"])}'
+        for assembly in self.metaschema.get("define-assembly", []):
+            local_refs[f'{_pythonize_name(assembly["name"])}'] = (
+                f'{_pythonize_name(assembly["formal-name"])}'
             )
-        # return empty list if key does not exist
-        for field in self.metaschema_dict.get("define-field", []):
-            self.local_methods[field["@name"]] = (
-                f'{self.metaschema_dict._get_class_name(field["formal-name"])}'
+
+        for field in self.metaschema.get("define-field", []):
+            local_refs[f'{_pythonize_name(field["name"])}'] = (
+                f'{_pythonize_name(field["formal-name"])}'
             )
-        # return empty list if key does not exist
-        for flag in self.metaschema_dict.get("define-flag", []):
-            self.local_methods[flag["@name"]] = (
-                f'{self.metaschema_dict._get_class_name(flag["formal-name"])}'
+
+        for flag in self.metaschema.get("define-flag", []):
+            local_refs[f'{_pythonize_name(assembly["name"])}'] = (
+                f'{_pythonize_name(assembly["formal-name"])}'
             )
+
+        return local_refs
 
     def _generate_assembly(self, assembly_dict) -> dict[str, str]:
         assembly_class = {}
-        assembly_class["name"] = self.metaschema_dict._get_class_name(
-            assembly_dict["formal-name"]
-        )
+        assembly_class["name"] = _pythonize_name(assembly_dict["formal-name"])
         assembly_class["root-name"] = assembly_dict.get("root-name", None)
-        if "define-flag" in assembly_dict.keys():
-            pass
+        assembly_class["flags"] = []
+
+        for flag in assembly_dict.get("define-flag", []):
+            assembly_class["flags"].append(self._generate_flag(flag))
 
         return assembly_class
 
@@ -128,9 +217,33 @@ class ModuleGenerator:
     def _generate_field(self, field_dict) -> str:
         field_class = ""
         # Parse flag data, pass to jinja template and return generated string
+        # Fields should reference datatypes which will be generated from MetaschemaSet.datatypes
         return field_class
 
     def _generate_constraint(self, constraint_dict) -> str:
         constraint_class = ""
         # Parse constraint data, pass to jinja template and return generated string
         return constraint_class
+
+
+class ClassGenerator:
+    """
+    A class to convert an Assembly, Field or Flag into a dictionary that can be passed to a template.
+    """
+
+    def __init__(
+        self,
+        class_dict: dict,
+        type: Literal["assembly", "field", "flag"],
+        refs: list[str] = [],
+    ):
+        pass
+
+
+class DatatypeGenerator:
+    """
+    A class to convert the Metaschema Datatypes into referencable classes
+    """
+
+    def __init__(self):
+        pass
