f46e76ef6a5e57ba01a664d550218d49f1b31f2b
Robert Sherwood <robert.sherwood@credentive.com>
2024-07-22 13:59:02 +0000

    add jinja code and generate datatypes module

--- metaschema-python/metaschema_python/codegen/generate_classes.py
+++ metaschema-python/metaschema_python/codegen/generate_classes.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 import jinja2
-from dataclasses import dataclass
 from typing import NamedTuple, cast, Literal
 from pathlib import Path
 
-from ..core.schemaparse import MetaSchemaSet, MetaSchema
+from ..core.schemaparse import MetaSchemaSet, MetaSchema, DataType
+
+
+# Module functions and variables
 
 
 def _pythonize_name(name: str) -> str:
@@ -16,6 +18,12 @@ def _pythonize_name(name: str) -> str:
     """
     # Strip spaces, convert dashes to underscores
     return f'{name.replace(" ", "").replace("-","_")}'
+
+
+# Intialize the jinja environment
+jinja_env = jinja2.Environment(
+    loader=jinja2.PackageLoader(package_name="metaschema_python.codegen")
+)
 
 
 class GlobalRef(NamedTuple):
@@ -53,10 +61,14 @@ class PackageGenerator:
     """
 
     def __init__(
-        self, parsed_metaschemas: MetaSchemaSet, destination_directory: Path
+        self,
+        parsed_metaschemas: MetaSchemaSet,
+        destination_directory: Path,
+        package_name: str,
     ) -> None:
         # initialize the package
         self.destination = destination_directory
+        self.package_name = package_name
         self.module_generators: list[ModuleGenerator] = []
 
         # Get the global symbols from all the metaschemas in the MetaschemaSet
@@ -133,7 +145,6 @@ class ModuleGenerator:
         self.module_name = _pythonize_name(cast(str, self.metaschema["short-name"]))
         self.module_imports = []
         self.class_generators: list[ClassGenerator] = []
-        self.source = ""
 
         #
         # The first pass is to generate the list of elements imported by or defined in the metaschema so that we can
@@ -145,7 +156,7 @@ class ModuleGenerator:
             _import["@href"] for _import in self.metaschema.get("import", [])
         ]
 
-        module_refs = {}
+        module_refs: dict[str, str] = {}
         for schema in imported_schemas:
             module_refs.update(
                 {
@@ -164,14 +175,28 @@ class ModuleGenerator:
         # TODO: parse out definitions of fields, flags and assemblies
         for assembly in self.metaschema.get("define-assembly", []):
             self.class_generators.append(
-                self._generate_assembly(assembly_dict=assembly)
+                ClassGenerator(
+                    class_dict=assembly,
+                    type="assembly",
+                    refs=list(module_refs.keys()),
+                )
             )
 
         for field in self.metaschema.get("define-field", []):
-            self.fields.append(self._generate_field(field_dict=field))
+            self.class_generators.append(
+                ClassGenerator(
+                    class_dict=field,
+                    type="field",
+                )
+            )
 
         for flag in self.metaschema.get("define-flag", []):
-            self.flags.append(self._generate_flag(flag_dict=flag))
+            self.class_generators.append(
+                ClassGenerator(
+                    class_dict=flag,
+                    type=flag,
+                )
+            )
 
         # TODO: iterate through definitions and pass them to the appropriate jinja templates. for "@ref", include an import and reference. check global imports.
 
@@ -182,48 +207,21 @@ class ModuleGenerator:
         local_refs = {}
 
         for assembly in self.metaschema.get("define-assembly", []):
-            local_refs[f'{_pythonize_name(assembly["name"])}'] = (
+            local_refs[f'{_pythonize_name(assembly["@name"])}'] = (
                 f'{_pythonize_name(assembly["formal-name"])}'
             )
 
         for field in self.metaschema.get("define-field", []):
-            local_refs[f'{_pythonize_name(field["name"])}'] = (
+            local_refs[f'{_pythonize_name(field["@name"])}'] = (
                 f'{_pythonize_name(field["formal-name"])}'
             )
 
         for flag in self.metaschema.get("define-flag", []):
-            local_refs[f'{_pythonize_name(assembly["name"])}'] = (
+            local_refs[f'{_pythonize_name(assembly["@name"])}'] = (
                 f'{_pythonize_name(assembly["formal-name"])}'
             )
 
         return local_refs
-
-    def _generate_assembly(self, assembly_dict) -> dict[str, str]:
-        assembly_class = {}
-        assembly_class["name"] = _pythonize_name(assembly_dict["formal-name"])
-        assembly_class["root-name"] = assembly_dict.get("root-name", None)
-        assembly_class["flags"] = []
-
-        for flag in assembly_dict.get("define-flag", []):
-            assembly_class["flags"].append(self._generate_flag(flag))
-
-        return assembly_class
-
-    def _generate_flag(self, flag_dict) -> str:
-        flag_class = ""
-        # Parse flag data, pass to jinja template and return generated string
-        return flag_class
-
-    def _generate_field(self, field_dict) -> str:
-        field_class = ""
-        # Parse flag data, pass to jinja template and return generated string
-        # Fields should reference datatypes which will be generated from MetaschemaSet.datatypes
-        return field_class
-
-    def _generate_constraint(self, constraint_dict) -> str:
-        constraint_class = ""
-        # Parse constraint data, pass to jinja template and return generated string
-        return constraint_class
 
 
 class ClassGenerator:
@@ -237,13 +235,91 @@ class ClassGenerator:
         type: Literal["assembly", "field", "flag"],
         refs: list[str] = [],
     ):
-        pass
+        # Call the appropriate function for the type, which will load and populate the template
+        if type == "assembly":
+            self._generate_assembly(assembly_dict=class_dict)
+        elif type == "field":
+            self._generate_field(field_dict=class_dict)
+        else:
+            self._generate_flag(flag_dict=class_dict)
+
+    def _generate_assembly(self, assembly_dict: dict[str, str]) -> dict[str, str]:
+        assembly_class = {}
+        assembly_class["name"] = _pythonize_name(assembly_dict["formal-name"])
+        assembly_class["root-name"] = assembly_dict.get("root-name", None)
+        assembly_class["flags"] = []
+
+        for flag in assembly_dict.get("define-flag", []):
+            assembly_class["flags"].append(self._generate_flag(flag))
+
+        return assembly_class
+
+    def _generate_flag(self, flag_dict) -> str:
+        flag_class = ""
+        # Parse flag data, pass to jinja template and return generated string
+        template_context = {}
+        template_context["base_class"] = "Flag"
+        template_context["class_name"] = flag_dict["@name"]
+        return flag_class
+
+    def _generate_field(self, field_dict) -> str:
+        field_class = ""
+        # Parse flag data, pass to jinja template and return generated string
+        # Fields should reference datatypes which will be generated from MetaschemaSet.datatypes
+        template = jinja_env.get_template("field_class.py.jinja2")
+        # Parse field data, pass to jinja template and return generated string
+        template_context = {}
+        template_context["class_name"] = _pythonize_name(field_dict["formal-name"])
+        template_context["datatype"] = field_dict["@as-type"]
+        return field_class
+
+    def _generate_constraint(self, constraint_dict) -> str:
+        constraint_class = ""
+        # Parse constraint data, pass to jinja template and return generated string
+        return constraint_class
 
 
-class DatatypeGenerator:
+class DatatypesGenerator:
     """
-    A class to convert the Metaschema Datatypes into referencable classes
+    A class to convert the Metaschema Datatypes into classes
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, datatypes: list[DataType]):
+        # process the datatypes to make the template context to pass to the template
+        template_context = {}
+
+        # Metaschema datatypes can inherit from each other, or from an xml datatype.
+        # We only count the "parent" datatype for purposes of inheritance if a datatype inherits from a
+        # metaschema datatype.
+        metaschema_parents = [datatype.name for datatype in datatypes]
+
+        datatype_list = []
+        for datatype in datatypes:
+
+            # make the description a single line
+            if len(datatype.description) > 0:
+                description = "".join(datatype.description)
+            else:
+                description = None
+
+            # check to see if the parent is another metaschema type
+            if datatype.base_type in metaschema_parents:
+                parent = datatype.base_type
+                pattern = None
+            else:
+                parent = None
+                pattern = datatype.pattern
+
+            datatype_list.append(
+                {
+                    "name": datatype.name,
+                    "pattern": pattern,
+                    "description": description,
+                    "parent": parent,
+                }
+            )
+        template_context["datatypes"] = datatype_list
+
+        template = jinja_env.get_template("datatypes.py.jinja2")
+
+        self.datatype_module = template.render(template_context)
