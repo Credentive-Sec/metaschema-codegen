823db94b4c69d1ddc9dab2d13a9d8f712e5fdf29
Robert Sherwood <robert.sherwood@credentive.com>
2024-07-22 17:39:35 +0000

    Added Datatypes test

--- metaschema-python/tests/test_codegen.py
+++ metaschema-python/tests/test_codegen.py
@@ -1,4 +1,8 @@
-from metaschema_python.codegen.generate_classes import PackageGenerator
+from metaschema_python.codegen.generate_classes import (
+    PackageGenerator,
+    ModuleGenerator,
+    DatatypesGenerator,
+)
 
 
 class TestPackageGenerator:
@@ -9,4 +13,20 @@ class TestPackageGenerator:
         )
 
     def test_package_generator_contents(self, generated_package):
-        assert generated_package.package is not None
+        assert generated_package.package_name is not None and isinstance(
+            generated_package.package_name, str
+        )
+
+    def test_class_generator(self, generated_package):
+        assert isinstance(generated_package.module_generators, list)
+
+    def test_classes(self, generated_package):
+        for m_gen in generated_package.module_generators:
+            assert isinstance(m_gen, ModuleGenerator)
+
+
+class TestDatatypesGenerator:
+    def test_generate_datatypes(self, parsed_metaschema):
+        dt_gen = DatatypesGenerator(parsed_metaschema.datatypes)
+        print(dt_gen.datatype_module)
+        assert isinstance(dt_gen.datatype_module, str)
