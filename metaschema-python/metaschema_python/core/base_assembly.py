5bdbe3d0e2720a34c8a04ec5bc5d38edad8db30a
Robert Sherwood <robert.sherwood@credentive.com>
2024-08-14 10:20:00 +0000

    removing unused code

--- metaschema-python/metaschema_python/core/base_assembly.py
+++
@@ -1,7 +0,0 @@
-from .base_instance import MetaschemaInstance
-
-class MetaschemaAssembly(MetaschemaInstance):
-    """
-    BaseAssembly represents a generic Assembly in Metaschema. It is typically subclassed in a particular generated schema, 
-    but is currently used in the generic parser.
-    """
